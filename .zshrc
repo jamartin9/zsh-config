3c0399db28844e30ae447cc240a30d6e1eb314e5
Justin Martin <jaming@protonmail.com>
2020-04-06 13:02:50 -0400

    squashed p10k zinit guix-alias

--- .zshrc
+++ .zshrc
@@ -2,134 +2,32 @@
 #
 # zsh configuration
 
-export PATH="${PATH}:${HOME}/.cargo/bin:${HOME}/bin:$HOME/.local/bin:${HOME}/.npm-packages/bin:${HOME}/go/bin"
-export TERM="xterm-256color"
-CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 
-# custom alias
-[[ -f ${CUR_DIR}/aliases.sh ]] && source ${CUR_DIR}/aliases.sh
+export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
+export HISTFILE="${ZDOTDIR}/history"
 
-
-# fix light on my kbd
-[[ ! -f ${HOME}/.Xmodmap ]] && echo "add mod3 = Scroll_Lock" > ${HOME}/.Xmodmap
-
-# Setup zplugin
-ZPLG_HOME="${ZDOTDIR:-$CUR_DIR}/.zplugin"
-if [[ ! -d $ZPLG_HOME ]]; then
-    # clone zplugin
-    mkdir ${ZPLG_HOME}
-    chmod g-rwX ${ZPLG_HOME}
-    git clone --depth 1 https://github.com/zdharma/zplugin.git ${ZPLG_HOME}/bin
-    # load zplugin for module/plugins
-    source "${ZPLG_HOME}/bin/zplugin.zsh"
-    autoload -Uz _zplugin
-    (( ${+_comps} )) && _comps[zplugin]=_zplugin
-    # build zplugin module for compiling scripts/plugins and reports
-    zplugin module build
-    # load module
-    module_path+=( "${ZPLG_HOME}/bin/zmodules/Src" )
-    zmodload zdharma/zplugin
-else
-    # load module/zplugin
-    module_path+=( "${ZPLG_HOME}/bin/zmodules/Src" )
-    zmodload zdharma/zplugin
-    source "${ZPLG_HOME}/bin/zplugin.zsh"
-    autoload -Uz _zplugin
-    (( ${+_comps} )) && _comps[zplugin]=_zplugin
+# p10k instant prompt
+if [[ -r "${XDG_CACHE_HOME}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+    source "${XDG_CACHE_HOME}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-autoload -Uz zkbd # key config
-[[ ! -f ${ZDOTDIR:-$CUR_DIR}/.zkbd/${TERM}-${${DISPLAY:t}:-${VENDOR}-${OSTYPE}} ]] && zkbd
-source ${HOME}/.zkbd/${TERM}-${${DISPLAY:t}:-${VENDOR}-${OSTYPE}}
-[[ -n ${key[Backspace]} ]] && bindkey "${key[Backspace]}" backward-delete-char
-[[ -n ${key[Insert]} ]] && bindkey "${key[Insert]}" overwrite-mode
-[[ -n ${key[Home]} ]] && bindkey "${key[Home]}" beginning-of-line
-[[ -n ${key[PageUp]} ]] && bindkey "${key[PageUp]}" up-line-or-history
-[[ -n ${key[Delete]} ]] && bindkey "${key[Delete]}" delete-char
-[[ -n ${key[End]} ]] && bindkey "${key[End]}" end-of-line
-[[ -n ${key[PageDown]} ]] && bindkey "${key[PageDown]}" down-line-or-history
-[[ -n ${key[Up]} ]] && bindkey "${key[Up]}" up-line-or-search
-[[ -n ${key[Left]} ]] && bindkey "${key[Left]}" backward-char
-[[ -n ${key[Down]} ]] && bindkey "${key[Down]}" down-line-or-search
-[[ -n ${key[Right]} ]] && bindkey "${key[Right]}" forward-char
+export TERM="xterm-256color"
 
-# prompt theme
-zplugin ice silent wait'!0' atload'powerlevel9k_prepare_prompts' depth'1'
-zplugin light bhilburn/powerlevel9k
+# add bin packages
+export PATH="${HOME}/.cargo/bin:$HOME/.local/bin:${HOME}/.npm-packages/bin:${HOME}/go/bin:${PATH}"
 
-# history
-zplugin ice silent pick"history.zsh"
-zplugin snippet OMZ::lib/history.zsh
+# fix light on my kbd
+#[[ ! -f ${HOME}/.Xmodmap ]] && echo "add mod3 = Scroll_Lock" > ${HOME}/.Xmodmap
 
-# completion
-zplugin ice silent blockf wait'0' depth'1'
-zplugin light zsh-users/zsh-completions
 
-# auto suggest
-ZSH_AUTOSUGGEST_USE_ASYNC="y"
-zplugin ice silent wait'0' atload'_zsh_autosuggest_start' cloneopts'-b develop --single-branch' depth'1'
-zplugin light zsh-users/zsh-autosuggestions
+# custom aliases
+[[ -f ${ZDOTDIR}/aliases.sh ]] && source ${ZDOTDIR}/aliases.sh
 
-# syntax highlighting
-zplugin ice silent wait'0' atinit'zpcompinit; zpcdreplay' depth'1'
-zplugin light zdharma/fast-syntax-highlighting
 
-# prompt
-PROMPT_EOL_MARK=''
-
-# colors for prompt
-autoload -Uz colors && colors
-# prompts
-POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=""
-POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%{$fg[green]%}$> "
-POWERLEVEL9K_PROMPT_ON_NEWLINE=true
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time)
-POWERLEVEL9K_RPROMPT_ON_NEWLINE=false
-
-# context
-POWERLEVEL9K_CONTEXT_DEFAULT_BACKGROUND="black"
-POWERLEVEL9K_CONTEXT_DEFAULT_FOREGROUND="blue"
-POWERLEVEL9K_CONTEXT_ROOT_BACKGROUND="black"
-POWERLEVEL9K_CONTEXT_ROOT_FOREROUND="blue"
-POWERLEVEL9K_CONTEXT_SUDO_BACKGROUND="black"
-POWERLEVEL9K_CONTEXT_SUDO_FOREROUND="blue"
-POWERLEVEL9K_CONTEXT_REMOTE_BACKGROUND="black"
-POWERLEVEL9K_CONTEXT_REMOTE_FOREROUND="blue"
-POWERLEVEL9K_CONTEXT_REMOTE_SUDO_BACKGROUND="black"
-POWERLEVEL9K_CONTEXT_REMOTE_SUDO_FOREROUND="blue"
-DEFAULT_USER=$USER
-POWERLEVEL9K_ALWAYS_SHOW_CONTEXT=true
-
-# status
-POWERLEVEL9K_STATUS_CROSS=true
-
-# dir
-POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
-POWERLEVEL9K_SHORTEN_DELIMITER=".."
-POWERLEVEL9K_SHORTEN_STRATEGY="truncate_to_first_and_last"
-POWERLEVEL9K_DIR_HOME_BACKGROUND="black"
-POWERLEVEL9K_DIR_HOME_FOREGROUND="blue"
-POWERLEVEL9K_DIR_DEFAULT_BACKGROUND="black"
-POWERLEVEL9K_DIR_DEFAULT_FOREGROUND="blue"
-POWERLEVEL9K_DIR_HOME_SUBFOLDER_BACKGROUND="black"
-POWERLEVEL9K_DIR_HOME_SUBFOLDER_FOREGROUND="blue"
-POWERLEVEL9K_DIR_ETC_BACKGROUND="black"
-POWERLEVEL9K_DIR_ETC_FOREGROUND="blue"
-
-# time
-POWERLEVEL9K_TIME_FORMAT="%D{%H:%M:%S %m/%d/%y}"
-POWERLEVEL9K_TIME_BACKGROUND="black"
-POWERLEVEL9K_TIME_FOREGROUND="blue"
-
-# vcs
-POWERLEVEL9K_VCS_SHORTEN_LENGTH=7
-POWERLEVEL9K_VCS_SHORTEN_MIN_LENGTH=7
-POWERLEVEL9K_VCS_SHORTEN_STRATEGY="truncate_from_right"
-POWERLEVEL9K_VCS_SHORTEN_DELIMITER="..."
-POWERLEVEL9K_VCS_CLEAN_BACKGROUND="black"
-POWERLEVEL9K_VCS_CLEAN_FOREGROUND="blue"
-POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND="black"
-POWERLEVEL9K_VCS_UNTRACKED_FOREGROUND="blue"
-POWERLEVEL9K_VCS_MODIFIED_BACKGROUND="black"
-POWERLEVEL9K_VCS_MODIFIED_FOREGROUND="blue"
+_zsh_kbd_setup
+_zsh_zinit_setup
+_zsh_zinit_plugins
+_guix_opts default #guixProf default
--- aliases.sh
+++ aliases.sh
@@ -1,105 +1,270 @@
-#!/usr/bin/env bash
-#
-# Cludge of aliases
-#
-
-_alias_ls(){
-    if ! [ -x "$(command -v exa)" ]; then
-        ls "$@"
-    else
-        exa "$@"
-    fi
-}
-alias ls="_alias_ls"
-
-_alias_cat(){
-    if ! [ -x "$(command -v bat)" ]; then
-        cat "$@"
-    else
-        bat "$@"
-    fi
-}
-alias cat="bat"
-
-_alias_grep(){
-    if ! [ -x "$(command -v rg)" ]; then
-        grep "$@"
-    else
-        rg "$@"
-    fi
-}
-alias grep="rg"
-
-_alias_find(){
-    if ! [ -x "$(command -v fd)" ]; then
-        find "$@"
-    else
-        fd "$@"
-    fi
-}
-alias find="fd"
-
-alias perfRecCmd="perf record -g --call-graph dwarf $1 | stackcollapse-perf | flamegraph > flame.svg"
-alias perfScriptList="perf script -l"
-alias perfReport="perf report -g"
-alias perfTopSysCalls="perf top -e raw_syscalls:sys_enter -ns comm"
-alias perfProbeList="perf probe -l"
-alias perfSysCalls="perf stat -e raw_syscalls:sys_enter -I 1000 -a"
-alias perfList="perf list"
+#!/usr/bin/env zsh
 
 alias javaListProps="java -server -XX:+PrintFlagsFinal -XX:+UnlockDiagnosticVMOptions --version"
-alias javaToolOptions="export JAVA_TOOL_OPTIONS=-XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump.hprof -XX:StartFlightRecording=disk=true,dumponexit=true,filename=recording.jfr,maxsize=1024m,maxage=1d,settings=profile,path-to-gc-roots=true -Xlog:gc*=debug:file=gc.log:utctime,uptime,tid,level:filecount=10,filesize=128m -XX:NativeMemoryTracking=detail -XX:+PreserveFramePointer"
-_jcmd_pid_help(){
-    jcmd $(jps | grep $1 | awk '{print $1}') help
+alias javaToolOptions="export JAVA_TOOL_OPTIONS='-XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heapdump.hprof -XX:StartFlightRecording=disk=true,dumponexit=true,filename=recording.jfr,maxsize=1024m,maxage=1d,settings=profile,path-to-gc-roots=true -Xlog:gc:gc.log:utctime,uptime,tid,level:filecount=10,filesize=128m -XX:NativeMemoryTracking=detail -XX:+PreserveFramePointer'"
+
+#
+# GUIX
+#
+
+_guix_vars(){
+    if [ -z "${GUIX_PREV_ENV}" ]; then
+        export GUIX_PREV_ENV=($(env))
+    fi
+    if [ -z "${GUIX_MANIFEST_DIR}" ]; then
+        export GUIX_MANIFEST_DIR="${XDG_CONFIG_DIR:-$HOME/.config}/guix-manifests"
+    fi
+    if [ -z "${GUIX_EXTRA_PROFILES}" ]; then
+        export GUIX_EXTRA_PROFILES="${XDG_DATA_HOME:-$HOME/.local/share}/guix-extra-profiles"
+    fi
+    if [ -z "${GUIX_ACTIVE_MANIFESTS+x}" ]; then
+        export GUIX_ACTIVE_MANIFESTS=()
+    fi
+    if [ -z "${GUIX_LOCPATH}" ]; then
+        export GUIX_LOCPATH="${HOME}/.guix-profile/lib/locale"
+    fi
 }
-alias jcmdHelp="_jcmd_pid_help"
-
-_wine_steam_path(){
-    export WINEPATH="${STEAM_LIB}/common/${PROTON_VERSION}/dist/bin"
-}
-_steam_exports(){
-    export STEAM_LIB="/storage/SteamLibrary/steamapps"
-    export PROTON_VERSION="Proton\ 3.16"
-    _wine_steam_path
-}
-_wine_steam_prefix(){
-    export WINEPREFIX="${STEAM_LIB}/compatdata/${APP_ID}/pfx"
-}
-# Games
-_divinity_original_sin_2(){
-    # moves support tool to game and changes winecfg to win 10
-    _steam_exports
-    export APP_ID="435150"
-    export GAME_NAME="Divinity\ Original\ Sin\ 2"
-    _wine_steam_prefix
-
-    # back up support tool
-    mv "${STEAM_LIB}/common/${GAME_NAME}/DefEd/bin/SupportTool.exe" "${STEAM_LIB}/common/${GAME_NAME}/DefEd/bin/SupportTool.exe.bak"
-
-    # move app to support tool
-    cp "${STEAM_LIB}/common/${GAME_NAME}/DefEd/bin/EoCApp.exe" "${STEAM_LIB}/common/${GAME_NAME}/DefEd/bin/SupportTool.exe"
-
-    # back up bin directory
-    mv "${STEAM_LIB}/common/${GAME_NAME}/bin" "${STEAM_LIB}/common/${GAME_NAME}/bin.bak"
-
-    # link the DefEd bin dir
-    ln -s "${STEAM_LIB}/common/${GAME_NAME}/DefEd/bin" "${STEAM_LIB}/common/${GAME_NAME}/bin"
-
-    # set to windows 10
-    winecfg #winetricks --force xact
-}
-alias steamFixDivinity=_divinity_original_sin_2
-
-# sets the core of jade empire for performance
-_jade_empire(){
-    pid=""
-    while [[ "x$pid" == "x" ]]; do
-        pid=`pidof JadeEmpire.exe`
+_guix_list_manifests(){
+    echo "The following manifests are available:\ndefault"
+    for manifest in "${GUIX_MANIFEST_DIR}"/*.scm; do
+        local name=$(basename "${manifest%.*}")
+        echo "${name}"
     done
-    mask=""
-    while [[ "x$mask" == "x" ]]; do
-        mask=`taskset -p "$pid" | grep "mask: 1"`
-    done
-    taskset -p 4 "$pid"
 }
-alias steamFixJadeEmpire=_jade_empire
+_guix_install_profile(){
+    local arg="${1}"
+    local profile="${GUIX_EXTRA_PROFILES}/${arg}/${arg}"
+    if [ "${arg}" != "default" ] &&
+       [ ! -d "${profile}" ] &&
+       [ -f "${GUIX_MANIFEST_DIR}/${arg}.scm" ]; then
+        mkdir -p "${GUIX_EXTRA_PROFILES}/${arg}"
+        guix package -m "${GUIX_MANIFEST_DIR}/${arg}.scm" -p "${profile}"
+    fi
+}
+_guix_update_profile(){
+    local arg="${1}"
+    local profile="${GUIX_EXTRA_PROFILES}/${arg}/${arg}"
+    if [ "${arg}" = "default" ];then
+        guix pull
+        guix package -u
+    elif [ -f "${GUIX_MANIFEST_DIR}/${arg}.scm" ] &&
+         [ -d "${profile}" ]; then
+         guix package -m "${GUIX_MANIFEST_DIR}/${arg}.scm" -p "${profile}"
+    else
+        _guix_install_profile "${arg}"
+    fi
+}
+_guix_deactivate_profile(){
+    local arg="${1}"
+    local ref="GUIX_PREV_ENV_${arg}"
+    if [ ! -z "${(P)ref}" ]; then # the prev env exists # TODO: indirect variable expansion portable
+        for entry in $(echo "${(P)ref}"); do # unset vars of profile # TODO: indirect variable expansion portable
+            if [[ "${entry%%=*}" != GUIX_PREV_ENV.* ]] &&
+               [ "${entry%%=*}" != "GUIX_ACTIVE_MANIFESTS" ] ;then
+                unset "${entry%%=*}"
+            fi
+        done
+        unset "GUIX_PREV_ENV_${arg}"
+        for entry in $(echo "${GUIX_PREV_ENV}"); do # restore init env
+            export "${entry%%=*}"="${entry#*=}"
+        done
+        # active manifests again
+        for mani in $(echo "${GUIX_ACTIVE_MANIFESTS}"); do
+            local profile="${GUIX_EXTRA_PROFILE}/${mani}/${mani}"
+            if [ "${mani}" = "${arg}" ]; then # delete from active array
+                export GUIX_ACTIVE_MANIFESTS=("${GUIX_ACTIVE_MANIFESTS[@]/$arg}")
+            elif [ "${mani}" = "default" ]; then # load profiles
+                [ -f "${HOME}"/.guix-profile/etc/profile ] && . "${HOME}"/.guix-profile/etc/profile
+                [ -f "${HOME}"/.config/guix/current/etc/profile ] && . "${HOME}"/.config/guix/current/etc/profile
+            elif [ -f "$profile"/etc/profile ]; then
+                local GUIX_PROFILE="${profile}"
+                . "${GUIX_PROFILE}"/etc/profile
+            fi
+        done
+    fi
+}
+_guix_activate_profile(){
+    local arg="${1}"
+    for mani in $(echo "${GUIX_ACTIVE_MANIFESTS}"); do
+        if [ "${mani}" = "${arg}" ]; then
+            return # already active
+        fi
+    done
+    _guix_install_profile "${arg}"
+    local profile="${GUIX_EXTRA_PROFILES}/${arg}/${arg}"
+    if [ "${arg}" = "default" ];then
+        [ -f "${HOME}"/.guix-profile/etc/profile ] && . "${HOME}"/.guix-profile/etc/profile
+        [ -f "${HOME}"/.config/guix/current/etc/profile ] && . "${HOME}"/.config/guix/current/etc/profile
+        local stash="${GUIX_PREV_ENV}" # hide prev envs from being saved
+        unset GUIX_PREV_ENV
+        for mani in $(echo "${GUIX_ACTIVE_MANIFESTS}"); do
+            local ref="GUIX_PREV_ENV_${mani}"
+            local "stash_${mani}"="${(P)ref}" # TODO: indirect variable expansion portable
+            unset "GUIX_PREV_ENV_${mani}"
+        done
+        local prev=($(env)) # save profiles env
+        export "GUIX_PREV_ENV_${arg}"="${prev}"
+        export GUIX_PREV_ENV="${stash}" # restore prev envs
+        for mani in $(echo "${GUIX_ACTIVE_MANIFESTS}"); do
+            local ref="stash_${mani}"
+            export "GUIX_PREV_ENV_${mani}"="${(P)ref}" # TODO: indirect variable expansion portable
+        done
+        GUIX_ACTIVE_MANIFESTS+=($arg) # add to manifests
+        export GUIX_ACTIVE_MANIFESTS
+    elif [ -f "${profile}"/etc/profile ]; then
+        local GUIX_PROFILE="${profile}"
+        . "${GUIX_PROFILE}"/etc/profile
+        local stash="${GUIX_PREV_ENV}" # hide prev env from being saved
+        unset GUIX_PREV_ENV
+        for mani in $(echo "${GUIX_ACTIVE_MANIFESTS}"); do
+            local ref="GUIX_PREV_ENV_${mani}"
+            local "stash_${mani}"="${(P)ref}" # TODO: indirect variable expansion portable
+            unset "GUIX_PREV_ENV_${mani}"
+        done
+        local prev=($(env))
+        export "GUIX_PREV_ENV_${arg}"="${prev}"
+        export GUIX_PREV_ENV="${stash}" # restore prev env
+        for mani in $(echo "${GUIX_ACTIVE_MANIFESTS}"); do
+            local ref="stash_${mani}"
+            export "GUIX_PREV_ENV_${mani}"="${(P)ref}" # TODO: indirect variable expansion portable
+        done
+        GUIX_ACTIVE_MANIFESTS+=($arg)
+        export GUIX_ACTIVE_MANIFESTS
+    fi
+}
+_guix_opts(){
+    local help="
+     Takes a list of manifest shortnames/commands
+
+     GUIX_MANIFEST_DIR will be set to ~/.guix-manifests/ when not set
+     GUIX_EXTRA_PROFILES will be set to ~/.guix-extra-profiles/ when not set
+     GUIX_ACTIVE_MANIFESTS will be set to the profiles that are activated (in order)
+     GUIX_PREV_ENV will be set to contents of env without profiles
+     GUIX_PREV_ENV_shortname will be set to the contents of env with the profile
+
+     The shortname is the manifest basename to use
+     GUIX_MANIFEST_DIR/shortname.scm is the format of manifest search path
+     Profiles for manifest will be stored under: GUIX_EXTRA_PROFILES/shortname/shortname
+
+     The commands are activate, update, deactivate and list; activate is the default
+     -a|--activate shortname -> sources/installs profile; appends to GUIX_ACTIVE_MANIFESTS
+     -d|--deactivate shortname -> restore env before the profile was activated
+     -u|--update shortname -> guix package upgrades profile
+     -l|--list -> print the contents of GUIX_MANIFEST_DIR as shortnames
+     -h|--help -> print this message
+"
+    _guix_vars
+    while [ $# -gt 0 ]; do
+        local arg="${1}"
+        case "${arg}" in
+            -a|--activate)
+                _guix_activate_profile "${2}"
+                shift 2
+                ;;
+            -d|--deactivate)
+                _guix_deactivate_profile "${2}"
+                shift 2
+                ;;
+            -h|--help)
+                echo "${help}"
+                shift
+                ;;
+            -l|--list)
+                _guix_list_manifests
+                shift
+                ;;
+            -u|--update)
+                _guix_update_profile "${2}"
+                shift 2
+                ;;
+            *)
+                _guix_activate_profile "${1}"
+                shift
+                ;;
+        esac
+    done
+}
+alias guixProf=_guix_opts
+
+#
+# ZSH
+#
+
+_zsh_kbd_setup(){
+    # key config
+    [[ ! -f ${ZDOTDIR:-$HOME}/.zkbd/${TERM}-${${DISPLAY:t}:-${VENDOR}-${OSTYPE}} ]] && autoload -Uz zkbd && zkbd
+    . ${ZDOTDIR:-$HOME}/.zkbd/${TERM}-${${DISPLAY:t}:-${VENDOR}-${OSTYPE}}
+    [[ -n ${key[Backspace]} ]] && bindkey "${key[Backspace]}" backward-delete-char
+    [[ -n ${key[Insert]} ]] && bindkey "${key[Insert]}" overwrite-mode
+    [[ -n ${key[Home]} ]] && bindkey "${key[Home]}" beginning-of-line
+    [[ -n ${key[PageUp]} ]] && bindkey "${key[PageUp]}" up-line-or-history
+    [[ -n ${key[Delete]} ]] && bindkey "${key[Delete]}" delete-char
+    [[ -n ${key[End]} ]] && bindkey "${key[End]}" end-of-line
+    [[ -n ${key[PageDown]} ]] && bindkey "${key[PageDown]}" down-line-or-history
+    [[ -n ${key[Up]} ]] && bindkey "${key[Up]}" up-line-or-search
+    [[ -n ${key[Left]} ]] && bindkey "${key[Left]}" backward-char
+    [[ -n ${key[Down]} ]] && bindkey "${key[Down]}" down-line-or-search
+    [[ -n ${key[Right]} ]] && bindkey "${key[Right]}" forward-char
+}
+alias zsh_kbd_setup=_zsh_kbd_setup
+
+_zsh_zinit_setup(){
+    # Setup zinit
+    local ZINIT_HOME="${ZDOTDIR:-$HOME}/.zinit"
+    if [[ ! -d $ZINIT_HOME ]]; then
+        # clone zinit
+        mkdir ${ZINIT_HOME}
+        chmod g-rwX ${ZINIT_HOME}
+        git clone --depth 1 https://github.com/jamartin9/zinit.git ${ZINIT_HOME}/bin
+        # load zinit for module/plugins
+        . "${ZINIT_HOME}/bin/zinit.zsh"
+        autoload -Uz _zinit
+        (( ${+_comps} )) && _comps[zinit]=_zinit
+        # build zinit module for compiling scripts/plugins and reports
+        zinit module build
+        # load module
+        module_path+=( "${ZINIT_HOME}/bin/zmodules/Src" )
+        zmodload zdharma/zplugin
+    else
+        # load module/zinit
+        module_path+=( "${ZINIT_HOME}/bin/zmodules/Src" )
+        zmodload zdharma/zplugin
+        . "${ZINIT_HOME}/bin/zinit.zsh"
+        autoload -Uz _zinit
+        (( ${+_comps} )) && _comps[zinit]=_zinit
+    fi
+}
+alias zsh_zinit_setup=_zsh_zinit_setup
+
+_zsh_zinit_plugins(){
+    # burst plugins
+    #@zinit-scheduler burst
+
+    # prompt theme
+    typeset -g PROMPT_EOL_MARK=''
+    zinit ice silent depth'1' atload"!. ${ZDOTDIR}/.p10k.zsh" nocd blockf #wait'!'
+    zinit light romkatv/powerlevel10k
+
+    # history
+    zinit ice silent pick"history.zsh"
+    zinit snippet OMZ::lib/history.zsh
+
+    # completion
+    zinit ice silent blockf wait'0' depth'1'
+    zinit light zsh-users/zsh-completions
+
+    # auto suggest
+    zinit ice silent wait'0' atload'_zsh_autosuggest_start' cloneopts'-b develop --single-branch' depth'1'
+    zinit light zsh-users/zsh-autosuggestions
+
+    # syntax highlighting
+    zinit ice silent wait'0' atinit'zicompinit; zicdreplay' depth'1'
+    zinit light zdharma/fast-syntax-highlighting
+
+    # zsh
+    #zplugin ice id-as"zsh" \
+        #        atclone"./.preconfig; CFLAGS=\"-I/usr/include -I/usr/local/include -g -O2 -Wall\" ./configure --prefix=\"$ZPFX\"" \
+        #        atpull"%atclone" make"install"
+    #zplugin load/update zsh
+}
+alias zsh_zinit_plugins=_zsh_zinit_plugins
